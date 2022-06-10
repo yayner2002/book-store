260203aaf46ab5a7e1d252b71ddb7a0e44f658db
yayner <yayner2002@gmail.com>
2022-06-10 15:00:34 +0300

    Add styling

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,29 +1,26 @@
-import { Link } from 'react-router-dom';
-import React from 'react';
-
+import { Link } from "react-router-dom";
+import React from "react";
 
 function Nav() {
-    const navStyle = {
-        color:'#9e9ea7',
-        textDecoration:'none',
-        cursor:'pointer'
-    }
+  const navStyle = {
+    color: "#9e9ea7",
+    textDecoration: "none",
+    cursor: "pointer",
+  };
   return (
-   <nav>
+    <nav>
       <Link to="/" style={navStyle}>
-      <h1 className='flex-left'>
-           BookStore
-       </h1>
+        <h1 className="flex-left">BookStore</h1>
       </Link>
-       <ul className='nav-links'>
-        <Link style={navStyle} to="/books" >
-            <li>Books</li>
+      <ul className="nav-links">
+        <Link style={navStyle} to="/books">
+          <li>Books</li>
         </Link>
-        <Link to="/categories" style={navStyle} >
-              <li>Categories</li>
+        <Link to="/categories" style={navStyle}>
+          <li>Categories</li>
         </Link>
-       </ul>
-   </nav>
+      </ul>
+    </nav>
   );
 }
 
