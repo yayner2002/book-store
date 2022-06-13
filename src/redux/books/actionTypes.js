e591fb3f10d75f7e7f13de7156cdfc535ab1729a
yayner <yayner2002@gmail.com>
2022-06-13 22:38:32 +0300

    Add book's actions and reducer

---
+++ src/redux/books/actionTypes.js
@@ -0,0 +1,2 @@
+export const BOOK_ADDED = 'bookAdded';
+export const BOOK_REMOVED = 'bookRemoved';
---
+++ src/redux/books/books.js
@@ -0,0 +1,28 @@
+import * as actions from './actionTypes';
+
+let lastId = 0;
+export const bookAdded = (title, author) => ({
+  type: actions.BOOK_ADDED,
+  payload: {
+    title,
+    author,
+  },
+});
+export default function reducer(state = [], action) {
+  switch (action.type) {
+    case actions.BOOK_ADDED:
+      return [
+        ...state,
+        {
+          // eslint-disable-next-line no-plusplus
+          id: ++lastId,
+          title: action.payload.title,
+          author: action.payload.author,
+        },
+      ];
+    case actions.BOOK_REMOVED:
+      return state.filter((book) => book.id !== action.payload.id);
+    default:
+      return state;
+  }
+}
