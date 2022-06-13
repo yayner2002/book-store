472f4590baa41235922c9cf1db5bf8ea8cd517ee
yayner <yayner2002@gmail.com>
2022-06-13 23:06:55 +0300

    Add categories actions and reducers

---
+++ src/redux/categories/categories.js
@@ -0,0 +1,20 @@
+import * as actions from './actionTypes';
+
+const defaultState = [];
+// actions creators
+export const checkStatus = () => ({
+  type: actions.CHECK_STATUS,
+  payload: {
+    description: 'Under construction',
+  },
+});
+
+// reducer
+export default function reducer(state = defaultState, action) {
+  switch (actions.type) {
+    case actions.CHECK_STATUS:
+      return action.payload.description;
+    default:
+      return state;
+  }
+}
