8f3849aee0a7c537ab3b676cfbdb6aad33ac75a4
yayner <yayner2002@gmail.com>
2022-06-15 18:17:05 +0300

    Refactor code

--- src/components/AddBook.js
+++ src/components/AddBook.js
@@ -7,6 +7,12 @@ function AddBook() {
   const [author, setAuthor] = useState('');
   const dispatch = useDispatch();
 
+  const handleTitle = (e) => {
+    setTitle(e.target.value);
+  };
+  const handleAuthor = (e) => {
+    setAuthor(e.target.value);
+  };
   const handleForm = () => {
     dispatch(bookAdded({ title, author }));
     setTitle('');
@@ -17,8 +23,8 @@ function AddBook() {
     <div className="form-container">
       <h3>Add New Book Here</h3>
       <form className="my-form" onSubmit={handleForm}>
-        <input className="form-control" type="text" name="title" placeholder="Enter Book Here" onChange={(e) => setTitle(e.target.value)} />
-        <input className="form-control" type="text" name="author" placeholder="Enter Book Author" onChange={(e) => setAuthor(e.target.value)} />
+        <input className="form-control" type="text" name="title" placeholder="Enter Book Here" onChange={handleTitle} />
+        <input className="form-control" type="text" name="author" placeholder="Enter Book Author" onChange={handleAuthor} />
         <button className="submit-btn" type="submit">Add Book</button>
       </form>
     </div>
