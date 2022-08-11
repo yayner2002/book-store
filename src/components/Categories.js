0233f074bbe7169286a1238ecda196dce40117e3
yayner <yayner2002@gmail.com>
2022-08-11 12:07:15 +0300

    Add some styles

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './styles/Categories.css';
 
 const Categories = () => (
   <div className="categories-container">
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,5 +1,6 @@
 import { Link } from 'react-router-dom';
 import React from 'react';
+import './styles/Nav.css';
 
 const Nav = () => {
   const navStyle = {
