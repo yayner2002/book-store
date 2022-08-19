482c4f7d4c174e74d16f230cf76d83410d4a048e
yayner <yayner2002@gmail.com>
2022-08-19 13:44:20 +0300

    Incorporate the comments from my reviewer

--- src/redux/categories/categories.js
+++ src/redux/categories/categories.js
@@ -1,19 +1,5 @@
 import { createSlice } from '@reduxjs/toolkit';
-// import CHECK_STATUS from './actionTypes';
 
-// const categoriesReducer = (state = '', action) => {
-//   switch (action.type) {
-//     case CHECK_STATUS:
-//       return action.payload;
-//     default:
-//       return state;
-//   }
-// };
-
-// export const checkStatus = () => ({
-//   type: CHECK_STATUS,
-//   payload: 'Under Construction',
-// });
 const initialState = {
   status: null,
 };
